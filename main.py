43550ef5a4b6ed0ee9f9453001d302c907c92deb
Aaron <aaronwenteler@gmail.com>
2021-12-26 17:53:38 +0100

    Calculate pairwise correlations for two arrays of feature data

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 import pandas as pd
+import numpy as np
+import os
 
-data = pd.read_pickle('inDelphi/test_FORECasT_inDelphi.pkl')
 
-del_feat = pd.DataFrame(data["del_features"])
+def get_pearson_ccs(feature_names, feature_data):
+    pearson_ccs = {}
+    feature_names_copy = feature_names.copy()
+    for feature_a in feature_names:
+        feature_names_copy.remove(feature_a)
+        feat_a_data = np.array(feature_data[feature_a].tolist())
+        for feature_b in feature_names_copy:
+            feat_b_data = np.array(feature_data[feature_b].tolist())
+            pearson_cc = np.corrcoef(feat_a_data, feat_b_data)[0, 1]
+            pearson_ccs[(feature_a, feature_b)] = pearson_cc
+
+    with open('pearson_ccs.pkl', 'wb') as f:
+        pd.to_pickle(pearson_ccs, f)
+
+    return pearson_ccs
+
+
+def main():
+    data = pd.read_pickle('inDelphi/test_FORECasT_inDelphi.pkl')
+
+    del_features = pd.DataFrame(data["del_features"])
+    del_features_cols = del_features.columns.tolist()
+
+    if os.path.exists('pearson_ccs.pkl'):
+        pearson_ccs = pd.read_pickle('pearson_ccs.pkl')
+    else:
+        pearson_ccs = get_pearson_ccs(del_features_cols, del_features)
+
+
+if __name__ == '__main__':
+    main()
