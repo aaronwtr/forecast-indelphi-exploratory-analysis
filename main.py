3860ea6b59c4d6e07324f078f988d579aae3a50c
Aaron <aaronwenteler@gmail.com>
2022-02-02 14:27:29 +0100

    Initialize heatmap implementation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
 import pearson_correlation as pearson
-
+import numpy as np
+import plotly.express as px
 
 def indelphi_pcc():
     data = pd.read_pickle('inDelphi/test_inDelphi.pkl')
@@ -17,9 +18,9 @@ def indelphi_pcc():
 
     significant_correlations = pearson.get_significant_correlations(pearson_ccs)
 
-    # for corr in significant_correlations:
-    #     if str(pearson.FEATURE_OUTPUT_PREFIX) + str(corr[0]) + '_' + str(corr[1]) + '.png' not in os.listdir(pearson.PEARSON_PLOT_OUTPUT_DIR):
-    #         pearson.scatter_plot(corr[0], corr[1], features, pearson_ccs)
+    pearson_ccs = {k: v for k, v in significant_correlations.items() if not pd.isna(v)}
+
+    return pearson_ccs
 
 
 def forecast_pcc():
@@ -32,7 +33,29 @@ def forecast_pcc():
     else:
         pearson_ccs = pearson.get_pearson_ccs(feature_cols, data, '', 'forecast')
 
-# TO-DO: Make heatmap of pearson correlation coefficients
+    significant_correlations = pearson.get_significant_correlations(pearson_ccs)
+
+    pearson_ccs = {k: v for k, v in significant_correlations.items() if not pd.isna(v)}
+
+    return pearson_ccs
+
+
+def heatmap(pccs, model):
+    feature_keys = list(pccs.keys())
+    feature_1 = [feature_keys[i][0] for i in range(len(feature_keys))]
+    feature_2 = [feature_keys[i][1] for i in range(len(feature_keys))]
+    feature_pccs = list(pccs.values())
+
+    df = pd.DataFrame(np.zeros((len(feature_1), len(feature_2))), index=feature_1, columns=feature_2).astype('float32')
+    long_df = pd.DataFrame({'Feature 1': feature_1, 'Feature 2': feature_2, 'Pearson Correlation Coefficent': feature_pccs})
+    df2 = long_df.pivot('Feature 1', 'Feature 2', 'Pearson Correlation Coefficent')
+    df.add(df2, fill_value=0).add(df2.T, fill_value=0)
+
+    fig = px.imshow(df, color_continuous_scale=px.colors.sequential.Plasma)
+    fig.update_layout(title='Pearson Correlation Heatmap of %s' % model)
+    fig.show()
+
+
 def main():
     """
     Note that the features for inDelphi are size, homologyLength and homologyGCContent.
@@ -42,10 +65,12 @@ def main():
     forecast = True
 
     if indelphi:
-        indelphi_pcc()
+        indelphi_pccs = indelphi_pcc()
+        heatmap(indelphi_pccs, 'inDelphi')
 
     if forecast:
-        forecast_pcc()
+        forecast_pccs = forecast_pcc()
+        heatmap(forecast_pccs, 'FORECasT')
 
 
 if __name__ == '__main__':
