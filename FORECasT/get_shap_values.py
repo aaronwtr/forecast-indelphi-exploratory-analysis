5257f97189a5a167dfc6f7f1f3e24cefc19c8219
Aaron <aaronwenteler@gmail.com>
2022-03-17 00:45:58 +0100

    Incorporated smooth pipeline for summary- and force plots

--- FORECasT/get_shap_values.py
+++ FORECasT/get_shap_values.py
@@ -55,7 +55,7 @@ def reshapeModelOutput(repair_outcome):
 
 
 def model(x):
-    feature_columns = list(model_df.columns)
+    feature_columns = list(background_df.columns)
     return predictionModel(x, DEFAULT_MODEL, feature_columns)
 
 
@@ -98,7 +98,8 @@ def getBackgroundData(guidedata, ioi):
         current_oligo = int(guidedata['ID'][oligo_idx][5:])
 
     samples = pd.read_pickle(
-        "FORECasT/train/Tijsterman_Analyser/" + str(guidedata['ID'][oligo_idx][0:5]) + '_' + str(current_oligo))
+        "FORECasT/train/Tijsterman_Analyser/" + str(guidedata['ID'][oligo_idx][0:5]) + '_' + str(current_oligo)
+    )
     proc_samples = getKernelExplainerModelInput(samples, current_oligo)
 
     oligo_idx = 0
@@ -113,7 +114,8 @@ def getBackgroundData(guidedata, ioi):
             continue
 
         feature_data = pd.read_pickle(
-            "FORECasT/train/Tijsterman_Analyser/" + str(guideset['ID'][oligo_idx][0:5]) + '_' + str(current_oligo))
+            "FORECasT/train/Tijsterman_Analyser/" + str(guideset['ID'][oligo_idx][0:5]) + '_' + str(current_oligo)
+        )
 
         model_df_temp = getKernelExplainerModelInput(feature_data, current_oligo)
         for index in model_df_temp.index:
@@ -134,11 +136,9 @@ def getBackgroundData(guidedata, ioi):
     return background_data
 
 
-def getExplanationData(guidedata):
-    indel_of_interest = "Oligo_58_D3_L-4C5R5"
-    indel_idx = indel_of_interest.split('_')[2]
-    oligo_of_interest = indel_of_interest.split('_')[1]
-    df_container = []
+def getExplanationData(guidedata, ioi):
+    indel_idx = ioi.split('_')[2]
+    oligo_of_interest = int(ioi.split('_')[1])
 
     oligo_idx = 0
     oligo_data = 0
@@ -149,10 +149,44 @@ def getExplanationData(guidedata):
         oligo_idx += 1
         current_oligo = int(guidedata['ID'][oligo_idx][5:])
 
-    proc_samples = getKernelExplainerModelInput(samples, current_oligo)
-    repair_outcome_of_interest = proc_samples.loc[indel_of_interest]  # note that this is a df series
+    samples = pd.read_pickle(
+        "FORECasT/train/Tijsterman_Analyser/" + str(guidedata['ID'][oligo_idx][0:5]) + '_' + str(current_oligo)
+    )
 
-    return
+    proc_samples = getKernelExplainerModelInput(samples, current_oligo)
+    backgroundIndels = list(background_df.index)
+
+    print('Collecting explanation data...')
+    explanation_data = pd.DataFrame(columns=proc_samples.columns)
+    pbar = tqdm(total=num_samples)
+    while oligo_data < num_samples:
+        current_oligo = guidedata['ID'][oligo_idx][5:]
+        oligo_name = str(guidedata['ID'][oligo_idx][0:5]) + '_' + str(current_oligo)
+        if oligo_name not in tijsterman_oligos:
+            oligo_idx += 1
+            continue
+
+        feature_data = pd.read_pickle(
+            "FORECasT/train/Tijsterman_Analyser/" + str(guideset['ID'][oligo_idx][0:5]) + '_' + str(current_oligo)
+        )
+
+        model_df_temp = getKernelExplainerModelInput(feature_data, current_oligo)
+        for index in model_df_temp.index:
+            df_indel_idx = index.split('_')[2]
+            if index in backgroundIndels:
+                continue
+
+            if df_indel_idx == indel_idx:
+                explanation_data.loc[index] = model_df_temp.loc[index]
+                oligo_data += 1
+                pbar.update(1)
+                break
+
+        oligo_idx += 1
+
+    pbar.close()
+
+    return explanation_data
 
 
 def getSHAPValue(model, background_data, explanation_data, explain_sample='all', link='logit'):
@@ -162,17 +196,15 @@ def getSHAPValue(model, background_data, explanation_data, explain_sample='all',
     is explained.
     """
 
-    num_samples = 10
-
     explainer = KernelExplainer(model, background_data, link=link)
 
     if explain_sample == 'all':
-        shap = explainer.shap_values(explanation_data, nsamples=num_samples)
+        shap = explainer.shap_values(explanation_data, nsamples="auto")
 
     elif explain_sample == 'one':
-        shap = explainer.shap_values(explanation_data.iloc[0, :], nsamples=num_samples)
+        shap = explainer.shap_values(explanation_data.iloc[0, :], nsamples="auto")
     else:
-        shap = explainer.shap_values(explanation_data.iloc[int(explain_sample), :], nsamples=num_samples)
+        shap = explainer.shap_values(explanation_data.iloc[int(explain_sample), :], nsamples="auto")
 
     return shap, explainer
 
@@ -190,63 +222,48 @@ if __name__ == '__main__':
     pred_data = []
 
     indel_of_interest = "Oligo_58_D3_L-4C5R5"
-    model_df = getBackgroundData(guideset, indel_of_interest)
-    model_input = model_df.to_numpy()  # SHAP expects a numpy array
 
-    print(model_df)
+    if os.path.isfile(f"FORECasT/background_datasets/{indel_of_interest}.pkl"):
+        background_df = pd.read_pickle(f"FORECasT/background_datasets/{indel_of_interest}.pkl")
+    else:
+        background_df = getBackgroundData(guideset, indel_of_interest)
+        background_df.to_pickle(f"FORECasT/background_datasets/{indel_of_interest}.pkl")
 
-    # oligo_data = 0
-    # dfs_container_ex = []
-    # explanation_data_size = 1000
-    # sample_size_explanation = int(np.round(explanation_data_size / num_oligos))
-    #
-    # print('\nCollecting explanation dataset...')
-    # # Get the explanation data set
-    # while oligo_data < num_oligos:
-    #     current_oligo = guideset['ID'][oligo_idx][5:]
-    #     oligo_name = str(guideset['ID'][oligo_idx][0:5]) + '_' + str(current_oligo)
-    #     if oligo_name not in tijsterman_oligos:
-    #         oligo_idx += 1
-    #         continue
-    #
-    #     feature_data = pd.read_pickle(
-    #         "FORECasT/train/Tijsterman_Analyser/" + str(guideset['ID'][oligo_idx][0:5]) + '_' + str(current_oligo))
-    #
-    #     model_df_temp = getKernelExplainerModelInput(feature_data, current_oligo, sample_size_explanation)
-    #     for i in range(len(list(model_df_temp.index))):
-    #         oligos_list.append(current_oligo)
-    #
-    #     dfs_container_ex.append(model_df_temp)
-    #     oligo_data += 1
-    #     oligo_idx += 1
+    if os.path.isfile(f"FORECasT/explanation_datasets/{indel_of_interest}.pkl"):
+        explanation_df = pd.read_pickle(f"FORECasT/explanation_datasets/{indel_of_interest}.pkl")
+    else:
+        explanation_df = getExplanationData(guideset, indel_of_interest)
+        explanation_df.to_pickle(f"FORECasT/explanation_datasets/{indel_of_interest}.pkl")
 
-    explain_prediction = 0
-    explanation_data_tmp = pd.concat(dfs_container_ex)
-    print('Explanation dataset: \n', explanation_data_tmp)
-    explanation_data = explanation_data_tmp.to_numpy()
-    explain_sample = 'all'
+    explain_prediction = 0  # note that the repair outcome of interest is in the first row of the explanation data
+    explain_sample = 'one'  # TODO make this a list of all and one and make a for loop to get both plots in one run
 
-    # shap_values, ex = getSHAPValue(model, model_df, explanation_data, explain_sample='all')
+    shap_values, ex = getSHAPValue(model, background_df, explanation_df, explain_sample=explain_sample)
 
-    # shap.initjs()
-    # plt.rcParams['ytick.labelsize'] = 'small'
-    #
-    # if explain_sample == 'one':
-    #     force = shap.force_plot(ex.expected_value, shap_values, model_df.iloc[explain_prediction, :], show=False)
-    #     shap.save_html(f'FORECasT/shap_values/force_plots/forceplot_{list(model_df.index)[explain_prediction]}.html',
-    #                    force)
-    #     plt.clf()
-    #     plt.close()
-    #
-    # if explain_sample == 'all':
-    #     shap.summary_plot(shap_values, model_df, list(model_df.columns))
-    #
-    # # save shap_values to .pkl file
-    # with open('FORECasT/shap_values/shapley_values/' + str(
-    #         list(model_df.index)[explain_prediction]) + '_' + 'num_background_data' + '_' +
-    #           str(num_oligos) + '.pkl', 'wb') as f:
-    #     pkl.dump(shap_values, f)
-    #     f.close()
+    shap.initjs()
+    plt.rcParams['ytick.labelsize'] = 'small'
+
+    if explain_sample == 'one':  # TODO explain variance in outcome of force plots
+        force = shap.force_plot(
+            ex.expected_value, shap_values, explanation_df.iloc[explain_prediction, :], list(explanation_df.columns),
+            show=False, contribution_threshold=0.1, text_rotation=0.4
+        )
+        shap.save_html(
+            f'FORECasT/shap_values_visualizations/force_plots/{list(explanation_df.index)[explain_prediction]}_forceplot.html',
+            force
+        )
+        plt.clf()
+        plt.close()
+
+    if explain_sample == 'all':
+        shap.summary_plot(shap_values, background_df, list(background_df.columns))
+
+    # save shap_values to .pkl file
+    with open('FORECasT/shap_values_visualizations/shapley_values/' + str(
+            list(explanation_df.index)[explain_prediction]) + 'kernel_explainer_approx_shapley_values' + '.pkl', 'wb'
+              ) as f:
+        pkl.dump(shap_values, f)
+        f.close()
 
     # TODO: Make the force plots for the repair outcomes with the highest probability of occuring.
 
